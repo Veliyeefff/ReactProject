0ac7a6ad33efe51d14f31267330cd5ebb23cdd9b
Veliyeefff <6mazgz0@code.edu.az>
2022-12-20 19:02:20 +0400

    Created

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,18 +1,6 @@
 import React, { useState } from "react";
 import { BsPhone } from "react-icons/bs";
 function Input() {
-  const [name, setname] = useState("");
-  const [email, setemail] = useState("");
-  const [number, setnumber] = useState();
-
-  const handleSubmit = (e) => {
-    e.preventDefeaul()
-      if(name && email && number){
-        
-      }
-  };
-
-  const
   return (
     <div className="input">
       <h3 className="input-text-h3">Let's Get In Touch!</h3>
@@ -21,10 +9,10 @@ function Input() {
         Ready to start your next project with us? Send us a messages and we will
         get back to you as soon as possible!
       </h4>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleName} placeholder="Full Name" />
-        <input type="email" onChange={handleEmail} placeholder="Email Address" />
-        <input type="text" onChange={handleNumber} placeholder="Phone Number" />
+      <form>
+        <input type="text"  placeholder="Full Name" />
+        <input type="email" placeholder="Email Address" />
+        <input type="text"  placeholder="Phone Number" />
         <textarea cols="30" rows="5" placeholder="Message"></textarea>
         <button className="submit">SUBMIT</button>
       </form>
